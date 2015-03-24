d99bf118d40bf35997671311281f5205998e2d72
root <root@localhost>
2015-03-24 08:19:05 -0500

    Configuracion Servidor

--- cls_Global.php
+++ cls_Global.php
@@ -17,7 +17,7 @@ class cls_Global {
     var $est_id='1';//
     var $pemi_id='1';//Punto Emision
     var $consumidorfinal='9999999999';
-    var $dateStartFact='2015-01-20';
-    var $limitEnv=2;
+    var $dateStartFact='2015-01-27';
+    var $limitEnv=10;
     
 }
