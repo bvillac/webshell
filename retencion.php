9544f87a742a44b38a15fc73a00528aefa139b01
root <root@developer.utimpor>
2016-06-29 10:39:19 -0500

    Actualiacion Retencion

--- retencion.php
+++ retencion.php
@@ -1,6 +1,6 @@
 <?php
 include('NubeRetencion.php');
 $obj = new NubeRetencion();
-//$res= $obj->insertarDocumentosFactura(1,'');
+$res= $obj->insertarDocumentosFactura(1,'');
 $res= $obj->insertarDocumentosPasivos(2,'');
 ?>
