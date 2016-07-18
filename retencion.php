2a75901c6d8de7e1be8ce51aaa88a3196e6dd0d6
root <root@developer.utimpor>
2016-07-18 15:45:31 -0500

    Modificacion de Retencion 2

--- retencion.php
+++ retencion.php
@@ -1,6 +1,6 @@
 <?php
 include('NubeRetencion.php');
 $obj = new NubeRetencion();
-//$res= $obj->insertarDocumentosFactura(1,'');//Compras alimenta inventarios
+$res= $obj->insertarDocumentosFactura(1,'');//Compras alimenta inventarios
 $res= $obj->insertarDocumentosPasivos(2,'');//Compras PROVICIONES
 ?>
