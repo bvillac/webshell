de1021f760c130e8abc461f98f6de71839c1081d
root <root@developer.utimpor>
2017-01-30 17:29:50 -0500

    Datos

--- envRecNc.php
+++ envRecNc.php
@@ -2,7 +2,7 @@
 include('NubeNotasCredito.php');//para HTTP
 $obj = new NubeNotasCredito();
 //$res= $obj->insertarDocumentosNC(1,'');
-//$res= $obj->enviarDocRecepcion();
-$res= $obj->enviarDocAutorizacion();
+$res= $obj->enviarDocRecepcion();
+//$res= $obj->enviarDocAutorizacion();
 ?>
 <?php //phpinfo() ?>
